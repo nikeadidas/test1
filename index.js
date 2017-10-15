66c0ec6aa4a74c7029ffe3f0cde076599f208f2c
nikeadidas <32810753+nikeadidas@users.noreply.github.com>
2017-10-15 12:45:01 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+function formSumbit(e) {
+	e.preventDefault();
+	var number = Number(e.target.num.value);
+	console.log('type:', typeof number);
+	console.log(number);
+}
+
+document.getElementById('my-form')
+.addEventListener('sumbit',formSumbit);
